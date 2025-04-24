82d037b096e2a691b6a84ec87eb995391db6a002
Ritika <ritikapriya376@gmail.com>
2025-04-24 17:25:21 +0530

    Pattern_12

--- Patterns_Problems/src/Pattern_11.java
+++ Patterns_Problems/src/Pattern_11.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Pattern_11 {
+    public static void main(String[] args) {
+        int num=1;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(num+"\t");
+                num++;
+            }
+
+            System.out.println();
+        }
+    }
 }
