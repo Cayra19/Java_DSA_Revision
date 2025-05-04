660a7883c60ca3ecbcd786b64cc6bdbf89e44bcc
Ritika <ritikapriya376@gmail.com>
2025-05-04 11:59:31 +0530

    Pattern_17 2nd method

--- Patterns_Problems/src/Pattern_17.java
+++ Patterns_Problems/src/Pattern_17.java
@@ -2,31 +2,31 @@ import java.util.Scanner;
 
 public class Pattern_17 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter your number: ");
-        int num=sc.nextInt();
-        int mid=num/2;
+        int num = sc.nextInt();
+        int mid = num / 2;
         System.out.println(mid);
-        int star=0;
-        for(int i=0;i<num;i++){
-            if(i==mid) {
-                for(int j=0;j<num;j++){
-                    System.out.print("*\t");
-                }
+        int star = 0;
+        for (int i = 0; i < num; i++) {
+//            if(i==mid) {
+//                for(int j=0;j<num;j++){
+//                    System.out.print("*\t");
+//                }
+//            }
+//            else {
+            for (int j = 0; j < mid; j++) {
+                if (i == mid) System.out.print("*\t");
+                else System.out.print("\t");
             }
-            else {
-                for (int j = 0; j < mid; j++) {
-                    System.out.print("\t");
-                }
-                for (int j = 0; j <= star; j++) {
-                    System.out.print("*\t");
-                }
+            for (int j = 0; j <= star; j++) {
+                System.out.print("*\t");
             }
-            if(i<mid){
+            //  }
+            if (i < mid) {
                 star++;
 
-            }
-            else star--;
+            } else star--;
             System.out.println();
         }
     }
