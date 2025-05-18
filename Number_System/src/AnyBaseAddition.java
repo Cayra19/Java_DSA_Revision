12bbbfe775e3ad24aebc12ae7a01bfc8c93f0135
Ritika <ritikapriya376@gmail.com>
2025-05-18 10:18:54 +0530

    AnyBaseAddition-2nd method

--- Number_System/src/AnyBaseAddition.java
+++ Number_System/src/AnyBaseAddition.java
@@ -2,25 +2,17 @@ import java.util.Scanner;
 
 public class AnyBaseAddition {
     public static int add(int num1, int num2, int base) {
-        int fNum = 0, sum = 0, pow = 0, remSum = 0, power = 0;
-        while (num1 != 0 && num2 != 0 && remSum!=1) {
-            int rem1 = (num1 % 10) + power;
+        int fNum = 0, quo = 0, pow = 0;
+        while (num1 != 0 || num2 != 0 || quo > 0) {
+            int rem1 = num1 % 10;
             int rem2 = num2 % 10;
             num1 /= 10;
             num2 /= 10;
-            remSum = rem1 + rem2;
-            System.out.println(remSum);
-            if (remSum >= base) {
-                sum = remSum - base;
-                power = 1;
-            } else {
-                sum = remSum;
-                power = 0;
-            }
-            fNum += sum * (int) Math.pow(10, pow);
-            if(num1==0 && num2==0 && power==1) fNum+= power* (int) Math.pow(10, pow+1);
+            int remSum = rem1 + rem2 + quo;
+            quo = remSum / base;
+            remSum %= base;
+            fNum += remSum * (int) Math.pow(10, pow);
             pow++;
-
         }
         return fNum;
     }
